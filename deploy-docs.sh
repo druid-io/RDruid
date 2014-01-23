9ed6d1c90ae7b5f669a81a24a4e0c75ca6f491fc
Xavier Léauté <xavier@metamarkets.com>
2014-01-23 13:18:18 -0800

    script to deploy documentation

---
+++ deploy-docs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+TARGET=deploy/docs
+if [ -d $TARGET ] ; then
+  git --git-dir=$TARGET/.git pull
+else
+  mkdir -p $TARGET
+  git clone -b gh-pages git@github.com:metamx/RDruid.git $TARGET
+fi
+
+rm -rf $TARGET/*.html $TARGET/css $TARGET/icons $TARGET/img $TARGET/js
+Rscript -e 'library(methods); staticdocs::build_package(".", "./deploy/docs")'
+
+git --git-dir=$TARGET/.git --work-tree=$TARGET add -A
